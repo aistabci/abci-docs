62b5634d02387a37dddd82c3dae59297b3300e85
Hirotaka Ogawa <h-ogawa@aist.go.jp>
2019-07-04 01:12:44 +0900

    append ABCI portal guide

--- site.mk
+++ site.mk
@@ -3,8 +3,12 @@ SITE_DIR := $(MKDOCS_ROOT)/site
 
 all:
 	mkdocs build --config-file $(MKDOCS_ROOT)/root/mkdocs.yml --site-dir ../site/
-	mkdocs build --config-file $(MKDOCS_ROOT)/ja/mkdocs.yml --site-dir ../site/ja/
-	mkdocs build --config-file $(MKDOCS_ROOT)/en/mkdocs.yml --site-dir ../site/en/
+	mkdocs build --config-file $(MKDOCS_ROOT)/ja/mkdocs.yml   --site-dir ../site/ja/
+	mkdocs build --config-file $(MKDOCS_ROOT)/en/mkdocs.yml   --site-dir ../site/en/
+
+	mkdocs build --config-file $(MKDOCS_ROOT)/portal/root/mkdocs.yml --site-dir ../../site/portal/
+	mkdocs build --config-file $(MKDOCS_ROOT)/portal/ja/mkdocs.yml   --site-dir ../../site/portal/ja/
+	mkdocs build --config-file $(MKDOCS_ROOT)/portal/en/mkdocs.yml   --site-dir ../../site/portal/en/
 
 clean:
-	echo rm -rf  $(SITE_DIR)
+	rm -rf  $(SITE_DIR)
